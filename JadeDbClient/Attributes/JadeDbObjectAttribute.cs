499275aabf3dcd50721ccc7247ce4a8b69d75ace
Kaustav Halder (KausCoder) <96991352+KaustavCodes@users.noreply.github.com>
2026-02-15 04:31:13 +0000

    Enhance JadeDbMapperGenerator and JadeDbObjectAttribute with improved mapping logic and documentation

--- JadeDbClient/Attributes/JadeDbObjectAttribute.cs
+++ JadeDbClient/Attributes/JadeDbObjectAttribute.cs
@@ -2,5 +2,9 @@ using System;
 
 namespace JadeDbClient.Attributes;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-public class JadeDbObjectAttribute : Attribute { }
+/// <summary>
+/// Marks a class or struct for automatic database mapping generation.
+/// The target must be declared as 'partial'.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false, AllowMultiple = false)]
+public sealed class JadeDbObjectAttribute : Attribute { }
